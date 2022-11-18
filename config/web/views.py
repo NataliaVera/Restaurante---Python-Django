6e16c4730dbc4ab7165cfbcfc9f0c9dba88134d2
Natalia Andrea Vera Torres <nataliavera2100@gmail.com>
2022-11-18 12:22:27 -0500

    clase 11 noviembre

--- config/web/views.py
+++ config/web/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from web.formularios.formularioPlatos import FormularioRegistroPlatos
+
 # Create your views here.
 #Cada vista es una funcion de python 
 
@@ -7,4 +9,20 @@ def Home(request):
     return render(request, 'index.html')
 
 def Platos(request):
-    return render(request, 'platos.html')
+    #cargar el formulario de registros de platos 
+    formulario = FormularioRegistroPlatos()
+
+    #creamos un diccionario para enviar datos hacia al template 
+    diccionarioEnvioDatos ={
+        'formulario':formulario
+    }
+
+    #RECIBIENDO DATOS DEL FORMULARIO 
+    #PETICION POST 
+    if request.method == 'POST':
+        datosFormulario = FormularioRegistroPlatos(request.POST)
+        
+        if datosFormulario.is_valid():
+            datosLimpios = datosFormulario.cleaned_data
+
+    return render(request, 'platos.html', diccionarioEnvioDatos)
